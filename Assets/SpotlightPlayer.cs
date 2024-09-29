791c9c9e29a31f83220b0e897bff9a5a191f6ca4
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2024-09-29 04:55:07 -0500

    some spotlight shiz

---
+++ Assets/SpotlightPlayer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpotlightPlayer : MonoBehaviour
+{
+    public float horizontalVelocity;
+    public float jumpHeight;
+    private Rigidbody2D rb;
+
+    private bool isGrounded = false;
+
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+
+    private float pushForce = 15;
+
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //hey wassup    
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        if(move != 0) {
+            rb.velocity = new Vector2(move * horizontalVelocity, rb.velocity.y);
+        }
+    }
+
+
+    public void OnSwitch() {
+        Debug.Log("we switched!");
+    }
+
+    public void Die()
+    {
+        Scene currentScene = SceneManager.GetActiveScene(); // Get the current scene
+        SceneManager.LoadScene(currentScene.name); // Reload the current scene
+    }
+
+}
